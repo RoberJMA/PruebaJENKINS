72470b41780f98d347b0d9cbf3b57d2ae311f802
RoberJMA <47140706+RoberJMA@users.noreply.github.com>
2024-05-28 00:15:10 +0200

    Update test_selenium.py

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,33 +1,24 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
-import unittest
-
-class TestWebApp(unittest.TestCase):
-
-    def setUp(self):
-        selenium_hub_url = 'http://selenium-hub:4444/wd/hub'
-        options = webdriver.ChromeOptions()
-        self.driver = webdriver.Remote(
-            command_executor=selenium_hub_url,
-            options=options
-        )
-
-    def test_example(self):
-        driver = self.driver
-        driver.get("https://192.168.56.10:30443")  # URL de la aplicación desplegada
-        self.assertIn("phpMyAdmin", driver.title)
-        elem = driver.find_element(By.NAME, "pma_username")
-        elem.send_keys("root")
-        elem = driver.find_element(By.NAME, "pma_password")
-        elem.send_keys("root")
-        elem.send_keys(Keys.RETURN)
-        time.sleep(5)
-        self.assertIn("phpMyAdmin", driver.title)
-
-    def tearDown(self):
-        self.driver.close()
-
-if __name__ == "__main__":
-    unittest.main()
+apiVersion: v1
+kind: Service
+metadata:
+  name: selenium-hub
+  labels:
+    app: selenium-hub
+spec:
+  ports:
+  - port: 4444
+    targetPort: 4444
+    nodePort: 31351 
+    name: port0
+  - port: 4443
+    targetPort: 4443
+    nodePort: 31352 
+    name: port1
+  - port: 4442
+    targetPort: 4442
+    nodePort: 31353  
+    name: port2
+  selector:
+    app: selenium-hub
+  type: NodePort
+  sessionAffinity: None
